7f7d024ea3de5e1b622cbd7db7c6c6d4c37bfe33
李昊谦 <lihaoqian@znlh.com>
2023-08-31 10:44:48 +0800

    【md】 杨辉三角 II

--- leetcode-xx11.js
+++ leetcode-xx11.js
@@ -27,8 +27,34 @@ const generate = (numRows) => {
   }
 };
 
+const getRow = (numRows) => {
+  const fn = (list) => {
+    const res = [1];
+    for (let i = 0; i < list.length - 1; i++) {
+      res.push(list[i] + list[i + 1]);
+    }
+    res.push(1);
+    return res;
+  };
+
+  if (numRows === 0) {
+    return [1];
+  } else if (numRows === 1) {
+    return [1, 1];
+  } else {
+    let index = 2;
+    let base = [[1], [1, 1]];
+    while (index <= numRows) {
+      base.push(fn(base[index - 1]));
+      index++;
+    }
+    return base[base.length - 1];
+  }
+};
+
 const list = [1, 2, 3, 4, 5, 6];
 
 list.forEach((item) => {
   console.log(generate(item));
+  console.log(getRow(item));
 });
